6578c5be2c9612280144f30176423493835737c2
rene <reneripardo@gmail.com>
2021-03-06 18:48:15 -0300

    plot images.

--- ct_processing/ct_image.py
+++ ct_processing/ct_image.py
@@ -1,5 +1,4 @@
 import cv2
-import numpy as np
 
 def mean_filter(image_in, kernel=3):
     image_out = cv2.blur(image_in, (kernel,kernel))
@@ -12,4 +11,13 @@ def median_filter(image_in, kernel=3):
 def gaussian_filter(image_in, kernel=3):
     image_out = cv2.GaussianBlur(image_in, (kernel,kernel), 0)
     return image_out
+
+def filter_laplacian(image_in):
+    image_out = cv2.Laplacian(image_in, cv2.CV_64F)
+    return image_out
+
+def filter_sobel(image_in, kernel=3):
+    image_outx = cv2.Sobel(image_in, cv2.CV_64F, 1, 0, ksize=kernel)
+    image_outy = cv2.Sobel(image_in, cv2.CV_64F, 0, 1, ksize=kernel)
+    return image_outx, image_outy
 
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from image_process.ct_processing.ct_image import median_filter, mean_filter, gaussian_filter
+from image_process.ct_processing.ct_image import median_filter, mean_filter, gaussian_filter, filter_laplacian, \
+    filter_sobel
 
 import argparse
 import matplotlib.pyplot as plt
@@ -7,17 +8,20 @@ import cv2
 
 def main():
     ap = argparse.ArgumentParser()
-    ap.add_argument('-i2', '--path_image', default='E:\\google_drive\\github\\repositories\\image_process\\img_example2.tif',
+    ap.add_argument('-i', '--path_image', default='E:\\google_drive\\github\\repositories\\image_process\\img_example.tif',
+                    help='path image example')
+    ap.add_argument('-i2', '--path_image2', default='E:\\google_drive\\github\\repositories\\image_process\\img_example2.tif',
                     help='path image example')
 
     args = vars(ap.parse_args())
 
     image_input = cv2.imread(args['path_image'],0)
+    image_input2 = cv2.imread(args['path_image2'], 0)
 
+    # low pass filter
     image_mean_filter = mean_filter(image_input, 5)
     image_median_filter = median_filter(image_input, 5)
     image_gaussian_filter = gaussian_filter(image_input, 5)
-
 
     plt.subplot(221), plt.imshow(image_input, cmap='gray')
     plt.title('Imagem original'), plt.xticks([]), plt.yticks([])
@@ -29,8 +33,19 @@ def main():
     plt.title('Filtro gaussiana (kernel=5x5)'), plt.xticks([]), plt.yticks([])
     plt.show()
 
+    # high pass filter
+    image_filter_laplacian = filter_laplacian(image_input2)
+    image_sobx, image_soby = filter_sobel(image_input2, 5)
 
-
+    plt.subplot(221), plt.imshow(image_input2, cmap='gray')
+    plt.title('Imagem original'), plt.xticks([]), plt.yticks([])
+    plt.subplot(222), plt.imshow(image_filter_laplacian, cmap='gray')
+    plt.title('Filtro laplaciano '), plt.xticks([]), plt.yticks([])
+    plt.subplot(223), plt.imshow(image_sobx, cmap='gray')
+    plt.title('Filtro sobel no eixo X (kernel=5x5)'), plt.xticks([]), plt.yticks([])
+    plt.subplot(224), plt.imshow(image_soby, cmap='gray')
+    plt.title('Filtro sobel no eixo Y (kernel=5x5)'), plt.xticks([]), plt.yticks([])
+    plt.show()
 
 if __name__ == "__main__":
     main()
