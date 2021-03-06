95bdbda813f3279235dc7828ef4bccac4fc274be
rene <reneripardo@gmail.com>
2021-03-06 13:34:42 -0300

    delete argument.

--- main.py
+++ main.py
@@ -7,12 +7,12 @@ import cv2
 
 def main():
     ap = argparse.ArgumentParser()
-    ap.add_argument('-i', '--path_image', default='E:\\google_drive\\github\\repositories\\image_process\\img_example_input.tif',
+    ap.add_argument('-i2', '--path_image', default='E:\\google_drive\\github\\repositories\\image_process\\img_example2.tif',
                     help='path image example')
 
     args = vars(ap.parse_args())
 
-    image_input = cv2.imread(args['path_image2'],1)
+    image_input = cv2.imread(args['path_image'],0)
 
     image_mean_filter = mean_filter(image_input, 5)
     image_median_filter = median_filter(image_input, 5)
